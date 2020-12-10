5a236e4945e546639e8f3be113555f8dcd7702aa
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2020-12-10 11:35:40 +0900

    Fix readme

--- errlist.h
+++ errlist.h
@@ -6,10 +6,12 @@
 #define ERR_CODE_INVALID_PAR_LOG_FILE   -504
 
 #define ERR_COMMAND_LINE        		"Wrong parameter(s)"
-#define ERR_OPEN_DEVICE         		"Error open device "
-#define ERR_CLOSE_DEVICE        		"Error open device "
+#define ERR_OPEN_SOCKET         		"open socket error "
+#define ERR_CLOSE_SOCKET        		"close socket error "
 #define ERR_BAD_STATUS          		"Bad status"
 #define ERR_INVALID_PAR_LOG_FILE		"Invalid log file "
+#define ERR_GET_ADDRESS					"get address info error "
+#define ERR_BIND						"bind error "
 
 #define MSG_INTERRUPTED 				"Interrupted "
 #define MSG_PG_CONNECTED        		"Connected"
@@ -17,5 +19,8 @@
 #define MSG_DAEMON_STARTED      		"Start daemon "
 #define MSG_DAEMON_STARTED_1    		". Check syslog."
 #define MSG_WS_TIMEOUT					"Web service time out"
+
+#define ERROR							"Error "
+
 
 const char *strerror_udpsplitter(int errcode);
